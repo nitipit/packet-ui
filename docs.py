4ec4412525b584728ce87e5b6f1673ba4f8fe158
Nitipit Nontasuwan <nitipit@gmail.com>
2021-04-03 20:03:19 +0700

    update

--- docs.py
+++ docs.py
@@ -18,34 +18,28 @@ def lib():
 
 
 async def engrave():
-    proc = await asyncio.create_subprocess_shell(
-        'engrave dev --server 127.0.0.1:8000 docs-src docs')
+    cmd = 'engrave dev --server 127.0.0.1:8000 docs-src docs'
+    proc = await asyncio.create_subprocess_shell(cmd)
     await proc.communicate()
 
 
-async def packet_ui_js():
-    src = _dir.joinpath('src/packet-ui.js')
-    dest_dir = _dir.joinpath('docs/_lib/packet-ui/')
+async def packet_ui():
+    src = _dir.joinpath('src').relative_to(_dir)
+    src = f'{src}/{{packet-ui.js,packet-ui.scss}}'
+    dest_dir = _dir.joinpath('docs/_lib/packet-ui/').relative_to(_dir)
+    cmd = f"npx parcel watch '{src}' --dist-dir='{dest_dir}' --watch-for-stdin"
+    print(cmd)
     proc = await asyncio.create_subprocess_shell(
-        f"npx parcel watch '{src}' --dist-dir='{dest_dir}'"
-    )
-    await proc.communicate()
-
-
-async def packet_ui_css():
-    src = _dir.joinpath('src/packet-ui.scss')
-    dest = _dir.joinpath('docs/_lib/packet-ui/packet-ui.css')
-    proc = await asyncio.create_subprocess_shell(
-        f"npx sass --watch {src} {dest}"
-    )
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,)
     await proc.communicate()
 
 
 async def main():
     lib()
     await asyncio.gather(
-        packet_ui_js(),
-        packet_ui_css(),
+        packet_ui(),
         engrave(),
     )
 
